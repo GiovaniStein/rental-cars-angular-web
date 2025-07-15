f8729cb3fbecc9b7db5557ee898e96cb26641c6c
GiovaniStein <giovanistein10@gmail.com>
2025-07-15 16:45:59 -0300

    fix: ajuste problema no router e ajuste breadcrumb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,16 @@ export const routes: Routes = [
         path: '', component: LayoutComponent, children: [
             {
                 path: '', loadComponent: () => import('./feature/home/home.component').then(component => component.HomeComponent)
+            },
+            {
+                path: 'relatorio', loadComponent: () => import('./feature/relatorio/relatorio.component').then(component => component.RelatorioComponent),
+                data: { breadcrumb: 'Relatórios' }
+            },
+            {
+                path: 'aluguel', loadComponent: () => import('./feature/aluguel/aluguel.component').then(component => component.AluguelComponent),
+                data: { breadcrumb: 'Alugueis' }
             }
         ]
     },
-    {
-        path: 'relatorio', loadComponent: () => import('./feature/relatorio/relatorio.component').then(component => component.RelatorioComponent)
-    },
-    {
-        path: 'aluguel', loadComponent: () => import('./feature/aluguel/aluguel.component').then(component => component.AluguelComponent)
-    }
+
 ];
